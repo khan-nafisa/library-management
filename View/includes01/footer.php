47e535ef25943daafad78d0e796ad6bd0e2c94aa
Khan Nafisa <khannafisa619@gmail.com>
2021-04-29 22:53:56 +0600

    library management

---
+++ View/includes01/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Brac University Library Management System |<a  href="https://Bracu.com/" target="_blank"  > Designed by : Farhana Khan</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>  
